7f28289bd2683a7a98d0b5d2f84444ec59cb3dca
zhangkai <zhangkai.gis@163.com>
2015-10-14 10:01:57 +0800

    read offline file

--- voipdetector/voipdetector/test.cpp
+++ voipdetector/voipdetector/test.cpp
@@ -9,21 +9,22 @@ int main(){
 	pcap_t * fd;
 
 	char errbuf[ERRBUFSIZ];
-	if(pcap_findalldevs(&alldevs, errbuf) == -1)
-	{
-		fprintf(stderr,"Error in pcap_findalldevs_ex: %s\n", errbuf);
-		exit(1);
-	}
-	//if(pcapfd = initpcap(alldevs->name,1,"",errbuf) == -1){
-	fd = initpcap("",1,"tcp",errbuf);
+//	if(pcap_findalldevs(&alldevs, errbuf) == -1)
+//	{
+//		fprintf(stderr,"Error in pcap_findalldevs_ex: %s\n", errbuf);
+//		exit(1);
+//	}
+//	//if(pcapfd = initpcap(alldevs->name,1,"",errbuf) == -1){
+//	fd = initpcap("",1,"tcp",errbuf);
 //	if(pcapfd == -1){
 //		fprintf(stdout,"%s\n",errbuf);
 //		exit(1);
 //	};
 	//add_to_select_set(pcapfd);
 	//sockets_run();
+	fd = pcap_open_offline("F:/docs/work/gateway/yingshi.pcap",errbuf);
 	int res;
-	u_int inum, i=0;
+	u_int inum = 0, i=0;
 	while((res = pcap_next_ex( fd, &header, &pkt_data)) >= 0)
 	{
 
@@ -32,15 +33,24 @@ int main(){
 			continue;
 
 		/* print pkt timestamp and pkt len */
-		printf("%ld:%ld (%ld)\n", header->ts.tv_sec, header->ts.tv_usec, header->len);			
+	//	printf("%ld:%ld (%ld)\n", header->ts.tv_sec, header->ts.tv_usec, header->len);			
 
-		/* Print the packet */
-		for (i=1; (i < header->caplen + 1 ) ; i++)
-		{
-			printf("%.2x ", pkt_data[i-1]);
-			if ( (i % 16) == 0) printf("\n");
+	//	/* Print the packet */
+	//	for (i=1; (i < header->caplen + 1 ) ; i++)
+	//	{
+	//		printf("%.2x ", pkt_data[i-1]);
+	//		if ( (i % 16) == 0) printf("\n");
+	//	}
+
+		
+		for (i = 1; i < header->caplen; i++){
+			if(pkt_data[i] == 0x67 && pkt_data[i+1] == 0x6c && pkt_data[i+2] == 0x73){
+				fprintf(stdout, "%d\n", inum);
+			}
 		}
 
-		printf("\n\n");		
+		//printf("\n\n");		
+		inum++;
 	}
+	system("pause");
 }
